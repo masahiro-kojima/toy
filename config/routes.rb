cf23cd853de05fdba51f468df58e50a65b968956
masahiro.kojima <mncc0220@gmail.com>
2020-08-03 19:59:38 +0000

    finish app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root'application#hello'
+  
+  resources:users
+  resources:microposts
+  # /にアクセスしたら/usersを開くようにする。
+  root'users#index'
+  
 end
